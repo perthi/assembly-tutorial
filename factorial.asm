31a5735d894fa2f05fdc2bb612f248ce0d499465
Per Thomas <pth@embc.no>
2020-09-13 15:43:58 +0200

    factorial

---
+++ factorial.asm
@@ -0,0 +1,15 @@
+        global  factorial
+
+        section .text
+factorial:
+        cmp     rdi, 1                  ; n <= 1?
+        jnbe    L1                      ; if not, go do a recursive call
+        mov     rax, 1                  ; otherwise return 1
+        ret
+L1:
+        push    rdi                     ; save n on stack (also aligns %rsp!)
+        dec     rdi                     ; n-1
+        call    factorial               ; factorial(n-1), result goes in %rax
+        pop     rdi                     ; restore n
+        imul    rax, rdi                ; n * factorial(n-1), stored in %rax
+        ret
