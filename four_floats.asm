73bb76c6598539c2025276874dd3ba2ef6f756f0
Per Thomas <pth@embc.no>
2020-09-13 15:52:41 +0200

    simd

---
+++ four_floats.asm
@@ -0,0 +1,9 @@
+        global   add_four_floats
+        section  .text
+
+add_four_floats:
+        movdqa   xmm0, [rdi]            ; all four values of x
+        movdqa   xmm1, [rsi]            ; all four values of y
+        addps    xmm0, xmm1             ; do all four sums in one shot
+        movdqa   [rdi], xmm0
+        ret
